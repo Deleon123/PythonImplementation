23f9be395331f3fb27e7a971269f40eeef8ae04c
Deleon123 <deleoncurso@gmail.com>
2022-05-24 18:34:15 -0300

    Arquivos baixados
    Os arquivos foram baixados, porém ainda devem ser compactados e a url deve ser descoberta automaticamente

--- teste1.py
+++ teste1.py
@@ -2,7 +2,7 @@
 import os
 import requests
 
-# descobrindo as urls de onde serão baixados os pdfs
+# descobrindo as urls de onde serão baixados os arquivos
 urls = [
     'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf',
     'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.xlsx',
@@ -10,7 +10,15 @@ urls = [
     'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_III_DC_2021_RN_465.2021.v2.pdf',
     'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_IV_PROUT_2021_RN_465.2021.v2.pdf'
 ]
+# diretório de saída
+output_dir = './Outputs'
 
 # baixando os pdfs
+for url in urls:
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_path = os.path.join(output_dir, os.path.basename(url))
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
 #compactando em zip
