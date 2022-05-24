45991026f199d328c82864a1022497b216690fa4
Deleon123 <deleoncurso@gmail.com>
2022-05-24 20:14:23 -0300

    Final teste 1
    Teste 1 finalizado, urls são buscadas automaticamente, arquivos são baixados no Output e arquivo compactado .zip é criado

--- teste1.py
+++ teste1.py
@@ -2,15 +2,17 @@
 import os
 import requests
 import zipfile
+from requests_html import HTML, HTMLSession
 
 # descobrindo as urls de onde serão baixados os arquivos
-urls = [
-    'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf',
-    'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.xlsx',
-    'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_II_DUT_2021_RN_465.2021_tea.br_RN473_RN477_RN478_RN480_RN513_RN536.pdf',
-    'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_III_DC_2021_RN_465.2021.v2.pdf',
-    'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_IV_PROUT_2021_RN_465.2021.v2.pdf'
-]
+link = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
+session = HTMLSession()
+r = session.get(link)
+# obtendo o url de cada arquivo (elementos filhos de 14 a 19)
+urls = []
+for index in range(14, 19):
+    page = r.html.find('#parent-fieldname-text > p:nth-child('+str(index)+') > a', first=True)
+    urls.append(page.absolute_links)
 # diretório de saída
 output_dir = './Outputs'
 
@@ -19,9 +21,11 @@ zip_file = zipfile.ZipFile('Outputs/archives.zip', 'w')
 
 # baixando os pdfs
 for url in urls:
-    response = requests.get(url)
+    # convertendo os SETS em string para o link ser baixado
+    strippedString = e = next(iter(url))
+    response = requests.get(strippedString)
     if response.status_code == 200:
-        file_path = os.path.join(output_dir, os.path.basename(url))
+        file_path = os.path.join(output_dir, os.path.basename(strippedString))
         with open(file_path, 'wb') as f:
             f.write(response.content)
             # colocando os arquivos em um zip 
