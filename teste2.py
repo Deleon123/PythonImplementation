9399bad25dec63a86aa46bfabbc40fd3e7921532
Deleon123 <deleoncurso@gmail.com>
2022-05-25 09:55:01 -0300

    Arquivo Compactado
    O arquivo CSV é armazenado em um arquivo compactado .zip dentro da pasta Outputs

--- teste2.py
+++ teste2.py
@@ -1,6 +1,13 @@
 # importando livarias necessárias
 import tabula
+import zipfile
 
 # Conversão do PDF em CSV
 tabula.convert_into('Outputs/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf','Outputs/Teste_Deleon.csv', output_format='csv', pages='all')
 
+# Compactando o arquivo CSV em .zip
+    # Criando o arquivo ZIP
+zipf = zipfile.ZipFile('Outputs/Teste_Deleon.zip', 'w', zipfile.ZIP_DEFLATED)
+    # guardando o arquivo dentro do ZIP
+zipf.write('Outputs/Teste_Deleon.csv')
+zipf.close()
