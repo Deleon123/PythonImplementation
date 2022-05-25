dfeb345a179c33b5962e1e46d96fa815c2af0019
Deleon123 <deleoncurso@gmail.com>
2022-05-25 10:18:16 -0300

    Teste 2 finalizado
    (remoção de uma livraria desnecessária)

--- teste2.py
+++ teste2.py
@@ -1,7 +1,6 @@
 # importando livarias necessárias
 import tabula
 import zipfile
-import re
 
 # Conversão do PDF em CSV
 tabula.convert_into('Outputs/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf','Outputs/Teste_Deleon.csv', output_format='csv', pages='all')
