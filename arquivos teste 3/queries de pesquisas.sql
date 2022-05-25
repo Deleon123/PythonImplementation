1c1bca17cab60461377588ed1334488770595b99
Deleon123 <deleoncurso@gmail.com>
2022-05-25 14:20:44 -0300

    QUERY TOP 10 operadoras
    10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre

--- arquivos teste 3/queries de pesquisas.sql
+++ arquivos teste 3/queries de pesquisas.sql
@@ -1,4 +1,7 @@
-SELECT * FROM demonstracoes_contabeis AS dc 
+SELECT roa.Razao_Social, COUNT(*) FROM demonstracoes_contabeis AS dc 
 JOIN relacao_de_operadoras_ativas AS roa 
 ON dc.REG_ANS = roa.Registro_ANS 
-where dc.DESCRICAO = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" and dc.Data_tempo = "2021-10-01"
+where dc.DESCRICAO = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" and dc.Data_tempo = "2021-10-01" 
+group by roa.Razao_Social 
+ORDER BY COUNT(*) DESC
+LIMIT 10
