7ed770847f6daaa1840d7ca18e4bb6788b863f9e
Deleon123 <deleoncurso@gmail.com>
2022-05-26 12:34:04 -0300

    TAREFA 3 CONCLUIDA
     -- Query que descobre as 10 empresas que mais tiveram gastos em EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR, no último ano (ano 2021) e  -- Query que descobre as 10 empresas que mais tiveram gastos em EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR, no último trimestre (data 2021-10-01)
    
    (além dos SQLs para importação e criação de tabela/banco de dados)

--- arquivos teste 3/query pesquisa 1.sql
+++ arquivos teste 3/query pesquisa 1.sql
@@ -1,7 +1,7 @@
 SELECT dc.REG_ANS, roa.Razao_Social, SUM(CAST(dc.VL_SALDO_FINAL as DECIMAL)) despesa FROM demonstracoes_contabeis_2021_4t AS dc 
 JOIN relacao_de_operadoras_ativas AS roa 
 ON dc.REG_ANS = roa.Registro_ANS 
-where dc.DESCRICAO = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" and CAST(dc.VL_SALDO_FINAL as DECIMAL) < 0
+where dc.CD_CONTA_CONTABIL = "41181" and CAST(dc.VL_SALDO_FINAL as DECIMAL) < 0 and Data_tempo = '2021-10-01'
 group by dc.REG_ANS 
 ORDER BY despesa ASC
 LIMIT 10
--- arquivos teste 3/query pesquisa 2.sql
+++ arquivos teste 3/query pesquisa 2.sql
@@ -1,16 +1,16 @@
-select dc.REG_ANS, roa.Razao_Social,sum(CAST(dc.VL_SALDO_FINAL as DECIMAL(10,2))) total
+select dc.REG_ANS, roa.Razao_Social,sum(CAST(dc.VL_SALDO_FINAL as float)) despesa
 from
 (
-    select REG_ANS,VL_SALDO_FINAL, DESCRICAO, Data_tempo 
+    select REG_ANS,VL_SALDO_FINAL, CD_CONTA_CONTABIL, Data_tempo 
     from demonstracoes_contabeis_2021_4t
     union all
-    select REG_ANS,VL_SALDO_FINAL, DESCRICAO, Data_tempo 
+    select REG_ANS,VL_SALDO_FINAL, CD_CONTA_CONTABIL, Data_tempo 
     from demonstracoes_contabeis_restante
 ) dc inner join relacao_de_operadoras_ativas AS roa 
 ON dc.REG_ANS = roa.Registro_ANS  
-where dc.DESCRICAO = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" and LEFT(dc.Data_tempo, 4) = "2021"
+where dc.CD_CONTA_CONTABIL = "41181" and (LEFT(dc.Data_tempo, 4) = "2021" or RIGHT(dc.Data_tempo, 4) = "2021" )and CAST(dc.VL_SALDO_FINAL as float) < 0
 group by dc.REG_ANS
-ORDER BY total ASC
+ORDER BY despesa ASC
 LIMIT 10
 
 -- Query que descobre as 10 empresas que mais tiveram gastos em EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR, no último ano (ano 2021)
